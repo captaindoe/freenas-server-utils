62bf512e8a4911f7ec8910787e872862146183af
Leo Ackerman <ackerman.leo@gmail.com>
2017-08-06 22:17:26 +0100

    curl path based on environment

--- apache2/cgi-bin/public_ip_location.pl
+++ apache2/cgi-bin/public_ip_location.pl
@@ -5,13 +5,15 @@ use strict;
 print "Status: 200\n";
 print "Content-type: application/json\n\n";
 
-my $curl = `which curl`;
-if (@? == -1) {
+my $curl;
+if (`uname` == 'FreeBSD') {
   $curl = '/usr/local/bin/curl';
 } else {
+  $curl = `which curl`;
   chomp $curl;
 }
 
 my $ip = `$curl -s ipecho.net/plain`;
 my $value = `$curl -s http://ip-api.com/json/$ip`;
+
 print "$value\n";
